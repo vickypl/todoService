e69e610a3cd9ff388f2531555c01a080a7d43698
vickypl <vicky542011@gmail.com>
2024-01-24 23:29:01 +0530

    added svc layer funcs

--- service/interfaces.go
+++ service/interfaces.go
@@ -5,7 +5,7 @@ import "github.com/todoService/model"
 type Todo interface {
 	Create(todo *model.Todo) (*model.Todo, error)
 	Get(filter model.Filter) ([]*model.Todo, error)
-	GetByID(id string) (*model.Todo, error)
+	GetByID(todoID string) (*model.Todo, error)
 	Update(todo *model.Todo) (*model.Todo, error)
-	Delete(id string) error
+	Delete(todoID string) error
 }
--- service/todo/todo.go
+++ service/todo/todo.go
@@ -1,1 +1,45 @@
 package todo
+
+import (
+	"github.com/todoService/model"
+	"github.com/todoService/service"
+	"github.com/todoService/store"
+)
+
+type todo struct {
+	todoStore store.Todo
+}
+
+func NewService(todoStore store.Todo) service.Todo {
+	return todo{todoStore: todoStore}
+}
+
+func (td todo) Create(todo *model.Todo) (*model.Todo, error) {
+	return td.todoStore.Create(todo)
+}
+
+func (td todo) Get(filter model.Filter) ([]*model.Todo, error) {
+	return td.todoStore.Get(filter)
+}
+
+func (td todo) GetByID(todoID string) (*model.Todo, error) {
+	return td.todoStore.GetByID(todoID)
+}
+
+func (td todo) Update(todo *model.Todo) (*model.Todo, error) {
+	todo, err := td.GetByID(todo.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return td.todoStore.Update(todo)
+}
+
+func (td todo) Delete(todoID string) error {
+	todo, err := td.GetByID(todoID)
+	if err != nil {
+		return err
+	}
+
+	return td.todoStore.Delete(todo.Id)
+}
